e17d5c9d73d4b9da9ee546044f858cc1b92c90b7
Adil Ansari <ansari.adil20@gmail.com>
2022-11-02 14:20:50 -0700

    Simplified client instantiation

--- lib/tigris.ts
+++ lib/tigris.ts
@@ -1,20 +1,12 @@
-import { DB, Tigris, TigrisClientConfig } from '@tigrisdata/core';
+import { DB, Tigris } from '@tigrisdata/core';
+import { Log } from '@tigrisdata/core/dist/utils/logger';
 
 const DB_NAME = 'nextjsTodoApp';
 
 if (!process.env.TIGRIS_URI) {
   throw new Error('Cannot find TIGRIS_URI environment variable ');
 }
-
-const tigrisUri = process.env.TIGRIS_URI;
-const clientConfig: TigrisClientConfig = { serverUrl: tigrisUri };
-
-if (process.env.TIGRIS_CLIENT_ID) {
-  clientConfig.clientId = process.env.TIGRIS_CLIENT_ID;
-}
-if (process.env.TIGRIS_CLIENT_SECRET) {
-  clientConfig.clientSecret = process.env.TIGRIS_CLIENT_SECRET;
-}
+Log.info(`Using Tigris at: ${process.env.TIGRIS_URI}`);
 
 declare global {
   // eslint-disable-next-line no-var
@@ -23,15 +15,15 @@ declare global {
 
 let tigrisDb: DB;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   // re-use the same connection in dev
   if (!global.tigrisDb) {
-    const tigrisClient = new Tigris(clientConfig);
+    const tigrisClient = new Tigris();
     global.tigrisDb = tigrisClient.getDatabase(DB_NAME);
   }
   tigrisDb = global.tigrisDb;
 } else {
-  const tigrisClient = new Tigris(clientConfig);
+  const tigrisClient = new Tigris();
   tigrisDb = tigrisClient.getDatabase(DB_NAME);
 }
 
