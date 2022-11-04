1ad5421afaf301605f81dd96ef005dd42b3585c8
Adil Ansari <adil@tigrisdata.com>
2022-11-03 17:21:46 -0700

    docs: Docs on client caching (#6)

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,6 +1,5 @@
 import { Tigris } from '@tigrisdata/core';
 import { loadEnvConfig } from '@next/env';
-import { Log } from '@tigrisdata/core/dist/utils/logger';
 
 // Run the config loader only when not executing within next runtime
 if (process.env.NODE_ENV === undefined) {
@@ -12,12 +11,7 @@ if (process.env.NODE_ENV === undefined) {
 }
 
 async function main() {
-  if (!process.env.TIGRIS_URI) {
-    console.log('Cannot find TIGRIS_URI environment variable ');
-    process.exit(1);
-  }
-  Log.info(`Using Tigris at: ${process.env.TIGRIS_URI}`);
-  // setup client & register schemas
+  // setup client and register schemas
   const tigrisClient = new Tigris();
   await tigrisClient.registerSchemas('models/tigris');
 }
